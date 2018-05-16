40b1c0b708ce1bb2495ddd592f49ce9be495d279
jangng9 <jang.ng9@gmail.com>
2018-05-16 15:11:08 +0700

    py

--- application/models.py
+++ application/models.py
@@ -1,6 +1,59 @@
 import os
-from application import app
 from flask import Flask, render_template, request
-
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "travelblog.db"))
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_flie
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+db.init_app(app)
+
+class Member_table(db.Model):
+    """User model."""
+    __tablename__ = 'MEMBER'
+    account_id = db.Column(db.Integer, primary_key=True)
+    account_name = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    picture = db.Column(db.String(255))
+
+    def __init__(self, account_name, username, password, picture)
+        self.username = username
+        self.password = password
+        self.account_name = account_name
+        self.picture = picture
+
+    def __repr__(self):
+        return "<Username: {}".format(self.username)
+
+"""
+class Favourite_table(db.Model):
+    """User Favourite"""
+    __tablename__ = 'FAVORITE'
+    fav_id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(255), nullable=False)    
+
+class File_Name_table(db.Model):
+    """File name"""
+    __tablename__ = 'FILE_NAME'
+    file_id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(255), db.ForeignKey(FAVORITE.file_name), nullable=False)
+
+    FAVORITE = relationship('Favourite_table')
+
+
+class User_Fav_table(db.Model):
+    """User Favoutite."""
+    __tablename__ = 'USER_FAV'
+    user_fav_id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey(MEMBER.account_id), nullable=False)
+    file_id = db.Column(db.Integer, db.ForeignKey(FILE_NAME.file_id), nullable=False)
+
+    MEMBER = relationship('Member_table')
+    FILE_NAME = relationship('File_Name_table')
+"""
 
--- application/views.py
+++ application/views.py
@@ -1,3 +1,36 @@
-from application import app
 from flask import render_template, flash, redirect, session, request, url_for
-from .models import 
+from .models import Member_table, db
+import os
+
+@app.route('/')
+
+@app.route('/index.html')
+def index():
+    username = session.get('username', '')
+    return render_template("index.html")
+
+@app.route('/header.html', method=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['rusename']
+        password = request.form['rpassword']
+        name = request.form['rname']
+        try:
+            new_user = Member_table(account_name = name, username = username, password = password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            session['password'] = password
+            flash('Register Successfully', 'success')
+            return redirect(url_for('.login'))
+        except:
+            db.session.rollback()
+            error = "Username or Password already exists."
+            flash('Something wrong!, please try again. ', 'error')
+    return render_template("register.html", error=error)
+
+@app.route('/logout')
+def logout():
+    session['username'] = ''
+    flash('You were logged out')
+    return redirect(url_for('index'))
