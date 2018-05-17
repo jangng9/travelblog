ff3fa389d6be536e8b18b68c9a086a44dbac46be
jangng9 <jang.ng9@gmail.com>
2018-05-17 18:29:37 +0700

    jub

--- travelblog/views.py
+++ travelblog/views.py
@@ -10,49 +10,6 @@ def index():
     username = session.get('username', '')
     return render_template("index.html")
     
-app.secret_key = os.urandom(12)
-@app.route('/login.html', methods=['GET','POST'])
-def login():
-    error = None
-    username = session.get('username','')
-    password = session.get('password','')
-    if request.method == 'POST':
-        users = Member_table.query.all()
-        for user in users:
-            if request.form['password'] == user.password and request.form['username'] == user.username:
-                flash('Login successfully.', 'success')
-                if username:
-                    session['username'] = username
-                else:
-                    session['username'] = request.form['username']
-                return redirect(url_for('.index'))
-            else:
-                error = 'Invalid username or password. Please try again.'
-    return render_template('login.html', error=error, username=username, password=password)
-    
-
-@app.route('/register.html', methods=['GET','POST'])
-def register():
-    error = None
-    if request.method == 'POST':
-        username = request.form['rusername']
-        password = request.form['rpassword']
-        name = request.form['rname']
-        picture = ""
-        try:
-            new_user = Member_table(account_name = name, username = username, password = password, picture=picture)
-            db.session.add(new_user)
-            db.session.commit()
-            session['username'] = username
-            session['password'] = password
-            flash(username)
-            flash('Register Successfully', 'success')
-            return render_template("login.html")
-        except Exception as e:
-            db.session.rollback()
-            flash(e)
-    return render_template("register.html", error=error)
-
 @app.route("/place_asiatique.html")
 def place_asiatique():
     username = session.get('username', '')
@@ -147,3 +104,53 @@ def type_natural():
 def type_religious():
     username = session.get('username', '')
     return render_template("type_religious.html")
+
+app.secret_key = os.urandom(12)
+@app.route('/login.html', methods=['GET','POST'])
+def login():
+    error = None
+    username = session.get('username','')
+    password = session.get('password','')
+    if request.method == 'POST':
+        users = Member_table.query.all()
+        for user in users:
+            if request.form['password'] == user.password and request.form['username'] == user.username:
+                flash('Login successfully.', 'success')
+                if username:
+                    session['username'] = username
+                else:
+                    session['username'] = request.form['username']
+                return redirect(url_for('.index'))
+            else:
+                error = 'Invalid username or password. Please try again.'
+    return render_template('login.html', error=error, username=username, password=password)
+    
+
+@app.route('/register.html', methods=['GET','POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        username = request.form['rusername']
+        password = request.form['rpassword']
+        name = request.form['rname']
+        picture = ""
+        try:
+            new_user = Member_table(account_name = name, username = username, password = password, picture=picture)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            session['password'] = password
+            flash(username)
+            flash('Register Successfully', 'success')
+            return render_template("login.html")
+        except:
+            db.session.rollback()
+            error = "Username or Password already exists."
+            flash('Something Wrong!', 'error')
+    return render_template("register.html", error=error)
+
+@app.route('/logout')
+def logout():
+    session['username'] = ''
+    flash('You were logged out')
+    return redirect(url_for('index'))
